4b3d00049d85ba24835f487bbfb50e1b978db401
michela <michelacatt@gmail.com>
2016-07-03 12:21:40 +0200

    Responsiveness kinda fixed. New mobile carousel.

--- File Vari/responsivecarousel.js
+++ File Vari/responsivecarousel.js
@@ -1,16 +1,6 @@
-$(document).ready(function() {
-	console.log($(document).width()/100);
-	var countTo = $(document).width()/100;
-	if (countTo > 10) {
-		countTo = 2;
-	}else {
-		countTo = 1;
-	}
-	
-	$('.carousel .item').each(function(){
-		for (var i = 0; i < countTo; i++) {
-			next = $(this).next();
-			next.appendTo($(this));
-		}
-	});
-});
+// Carousel Auto-Cycle
+  $(document).ready(function() {
+    $('.carousel').carousel({
+      interval: 6000
+    })
+  });
